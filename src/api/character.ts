e1171ae4a033fcd5799fe27f1acd3ffad61be52a
Alexander Neupokoev <admin@armavir.ru>
2021-03-21 08:52:01 +0300

    fix deploy

--- src/api/character.ts
+++ src/api/character.ts
@@ -3,8 +3,6 @@ import axios from 'axios'
 const instance = axios.create({
     baseURL: 'https://rickandmortyapi.com/api/character',
     withCredentials: true,
-    headers: {"Access-Control-Allow-Origin": "*"}
-
 })
 
 export const characterAPI = {
